7ab87922943e86f229c1c371717f32e7a088a4f3
Suraj Air <surajair@gmail.com>
2022-05-07 10:45:17 +0530

    More fine tuning

--- src/stories/Marketing/Features/Feature1.stories.jsx
+++ src/stories/Marketing/Features/Feature1.stories.jsx
@@ -1,6 +1,5 @@
 import { SectionWrapper } from "../../components/SectionWrapper";
-import { SectionSubHeading } from "../../components/SectionSubHeading";
-import { AiOutlineArrowRight, AiFillPlayCircle } from "react-icons/ai";
+import { AiFillPlayCircle, AiOutlineArrowRight } from "react-icons/ai";
 
 export const Feature1 = () => {
   return (
@@ -8,17 +7,13 @@ export const Feature1 = () => {
       <SectionWrapper>
         <div className="flex flex-col-reverse lg:grid lg:grid-cols-2">
           <div className="flex flex-col gap-y-5">
-            <h1 className="text-2xl font-semibold drop-shadow-lg dark:text-white lg:text-5xl">
-              A new and better way
-              <br className="hidden lg:inline" /> to acquire engage and
-              <br className="hidden lg:inline" /> retain customers
+            <h1 className="max-w-xl text-2xl font-semibold drop-shadow-lg dark:text-white lg:text-5xl">
+              A new and better way to acquire engage and retain customers
             </h1>
-            <SectionSubHeading>
-              Create screens directly in Method or add your images
-              <br className="hidden lg:inline" /> from sketch or Figma. You
-              caneven sync designs
-              <br className="hidden lg:inline" /> from your cloud storage!
-            </SectionSubHeading>
+            <p className={"max-w-sm text-lg"}>
+              Create screens directly in Method or add your images from sketch
+              or Figma. You can even sync designs from your cloud storage!
+            </p>
             <a
               href={"#"}
               className="bg-primary rounded-global font-xl flex w-fit items-center gap-x-3 p-3 text-white"
@@ -36,7 +31,7 @@ export const Feature1 = () => {
         </div>
         <div className="grid grid-cols-1 gap-x-8 text-left lg:grid-cols-3">
           <div className="">
-            <h1 className="text-xl font-semibold tracking-tight dark:text-white lg:text-2xl">
+            <h1 className="text-xl font-semibold dark:text-white lg:text-2xl">
               Email Marketing
             </h1>
             <p className="py-3 text-lg">
@@ -45,7 +40,7 @@ export const Feature1 = () => {
             </p>
           </div>
           <div className="">
-            <h1 className="text-xl font-semibold tracking-tight dark:text-white lg:text-2xl">
+            <h1 className="text-xl font-semibold dark:text-white lg:text-2xl">
               Marketing Automation
             </h1>
             <p className="py-3 text-lg">
@@ -54,7 +49,7 @@ export const Feature1 = () => {
             </p>
           </div>
           <div className="">
-            <h1 className="text-xl font-semibold tracking-tight dark:text-white lg:text-2xl">
+            <h1 className="text-xl font-semibold dark:text-white lg:text-2xl">
               Engage Customers
             </h1>
             <p className="py-3 text-lg">

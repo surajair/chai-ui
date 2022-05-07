27b2f70a3b46db502cbfd272af216c58047bfc38
Suraj Air <surajair@gmail.com>
2022-05-07 10:36:48 +0530

    Updated Featuer 1

--- src/stories/Marketing/Features/Feature1.stories.jsx
+++ src/stories/Marketing/Features/Feature1.stories.jsx
@@ -7,45 +7,36 @@ export const Feature1 = () => {
     <>
       <SectionWrapper>
         <div className="flex flex-col-reverse lg:grid lg:grid-cols-2">
-          <div className="">
+          <div className="flex flex-col gap-y-5">
             <h1 className="text-2xl font-semibold drop-shadow-lg dark:text-white lg:text-5xl">
               A new and better way
               <br className="hidden lg:inline" /> to acquire engage and
               <br className="hidden lg:inline" /> retain customers
             </h1>
-            <div className="py-2">
-              <SectionSubHeading>
-                Create screens directly in Method or add your images
-                <br className="hidden lg:inline" /> from sketch or Figma. You
-                caneven sync designs
-                <br className="hidden lg:inline" /> from your cloud storage!
-              </SectionSubHeading>
-            </div>
-            <div className="py-2">
-              <div className="bg-primary flex h-12 w-40 rounded-lg py-3 pl-3">
-                <a href="" className="font-xl text-white">
-                  Learn More
-                </a>
-                <a className="my-auto mx-auto text-white">
-                  <AiOutlineArrowRight />
-                </a>
-              </div>
-            </div>
+            <SectionSubHeading>
+              Create screens directly in Method or add your images
+              <br className="hidden lg:inline" /> from sketch or Figma. You
+              caneven sync designs
+              <br className="hidden lg:inline" /> from your cloud storage!
+            </SectionSubHeading>
+            <a
+              href={"#"}
+              className="bg-primary rounded-global font-xl flex w-fit items-center gap-x-3 p-3 text-white"
+            >
+              Learn More
+              <AiOutlineArrowRight />
+            </a>
           </div>
-          <div className="my-auto flex justify-center ">
-            <div className=" w-96 rounded bg-gray-200">
-              <div className="text-primary py-32  px-44">
-                <AiFillPlayCircle />
-              </div>
-            </div>
+          <div className="rounded-global my-auto flex h-full items-center justify-center bg-gray-100 dark:bg-gray-700 ">
+            <AiFillPlayCircle className={"text-primary text-5xl"} />
           </div>
         </div>
-        <div className="py-4">
+        <div className="py-8">
           <hr className={"dark:border-gray-800"} />
         </div>
-        <div className="grid grid-cols-1 text-left lg:grid-cols-3">
-          <div className="py-3">
-            <h1 className="text-xl font-semibold dark:text-white lg:text-2xl">
+        <div className="grid grid-cols-1 gap-x-8 text-left lg:grid-cols-3">
+          <div className="">
+            <h1 className="text-xl font-semibold tracking-tight dark:text-white lg:text-2xl">
               Email Marketing
             </h1>
             <p className="py-3 text-lg">
@@ -53,8 +44,8 @@ export const Feature1 = () => {
               the readable content of a page when looking at its layout.{" "}
             </p>
           </div>
-          <div className="py-3 lg:mx-4">
-            <h1 className="text-xl font-semibold dark:text-white lg:text-2xl">
+          <div className="">
+            <h1 className="text-xl font-semibold tracking-tight dark:text-white lg:text-2xl">
               Marketing Automation
             </h1>
             <p className="py-3 text-lg">
@@ -62,8 +53,8 @@ export const Feature1 = () => {
               the readable content of a page when looking at its layout.{" "}
             </p>
           </div>
-          <div className="py-3 lg:mx-4">
-            <h1 className="text-xl font-semibold dark:text-white lg:text-2xl">
+          <div className="">
+            <h1 className="text-xl font-semibold tracking-tight dark:text-white lg:text-2xl">
               Engage Customers
             </h1>
             <p className="py-3 text-lg">

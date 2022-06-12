055ee664e974e678cb3b89961b86feaac51efa94
MayurVejre <mayur.vejare27@gmail.com>
2022-06-12 21:07:52 +0530

    Teams 4 Added

---
+++ src/stories/Marketing/Teams/Team4.stories.jsx
@@ -0,0 +1,234 @@
+import { SectionWrapper } from "../../components/SectionWrapper";
+import { AiOutlineTwitter, AiOutlineGoogle } from "react-icons/ai";
+import { GrLinkedinOption } from "react-icons/gr";
+export const Team4 = () => {
+  return (
+    <>
+      <SectionWrapper>
+        <div className=" md:px-10">
+          <div className="hidden py-1 lg:inline  lg:flex">
+            <h1 className="text-3xl font-bold leading-7 md:text-5xl 2xl:text-6xl">
+              The Executive Team
+            </h1>
+            <div className="lg:pl-96">
+              <img src="https://res.cloudinary.com/dw8gdozpq/image/upload/v1655048145/Group_lkxoyg.png" />
+            </div>
+          </div>
+          <div className="lg:hidden">
+            <img src="https://res.cloudinary.com/dw8gdozpq/image/upload/v1655048145/Switcher_fjprmh.png" />
+          </div>
+          <div className="grid grid-cols-1 py-5 md:grid-cols-2 lg:grid-cols-4">
+            <div className="mx-2 my-2">
+              <div className="">
+                <p>CEO & Founder</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Dany Monson
+                </h1>
+              </div>
+              <hr className="my-3 w-80 md:w-60" />
+              <p>
+                The add or remove programs <br className="hidden lg:inline" />
+                utility is very useful, but not all{" "}
+                <br className="hidden lg:inline" />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 my-2">
+              <div className="">
+                <p>Front End Developer</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Dima Grinchenko
+                </h1>
+              </div>
+              <hr className="my-3 w-60" />
+              <p>
+                The add or remove programs <br className="hidden lg:inline" />
+                utility is very useful, but not all{" "}
+                <br className="hidden lg:inline" />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 my-2">
+              <div className="">
+                <p>Manager</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Polly Clark
+                </h1>
+              </div>
+              <hr className="my-3 w-60" />
+              <p>
+                The add or remove programs <br className="hidden lg:inline" />
+                utility is very useful, but not all{" "}
+                <br className="hidden lg:inline" />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 my-2 hidden md:inline">
+              <div className="">
+                <p>Front End Engineer</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Markus Johnson
+                </h1>
+              </div>
+              <hr className="my-3 w-60" />
+              <p>
+                The add or remove programs <br />
+                utility is very useful, but not all <br />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 my-2 hidden md:inline">
+              <div className="">
+                <p>Support</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Terry Valdez
+                </h1>
+              </div>
+              <hr className="my-3 w-60" />
+              <p>
+                The add or remove programs <br />
+                utility is very useful, but not all <br />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 my-2 hidden md:inline">
+              <div className="">
+                <p>Senior Developer</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Elen Joseph
+                </h1>
+              </div>
+              <hr className="my-3 w-60" />
+              <p>
+                The add or remove programs <br />
+                utility is very useful, but not all <br />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 my-2 hidden md:inline">
+              <div className="">
+                <p>Project Manager</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Iva Doyle
+                </h1>
+              </div>
+              <hr className="my-3 w-60" />
+              <p>
+                The add or remove programs <br />
+                utility is very useful, but not all <br />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 my-2 hidden md:inline">
+              <div className="">
+                <p>Art Director</p>
+                <h1 className="text-1xl font-semibold lg:text-2xl lg:font-bold">
+                  Baha Moretz
+                </h1>
+              </div>
+              <hr className="my-3 w-60" />
+              <p>
+                The add or remove programs <br />
+                utility is very useful, but not all <br />
+                of the programs are listed up.
+              </p>
+              <div className="flex py-3">
+                <div className="mr-2 py-2">
+                  <AiOutlineTwitter className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <GrLinkedinOption className="h-5 w-5" />
+                </div>
+                <div className="mx-2 py-2">
+                  <AiOutlineGoogle className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SectionWrapper>
+    </>
+  );
+};
+
+export default {
+  title: "Marketing/Teams/Team 4",
+  component: Team4,
+};
